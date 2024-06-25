396c0df29564528e56dcbb830c369e529634cb8e
guilherme.monteiro.espim@gmail.com <guilherme.monteiro.espim@gmail.com>
2024-06-25 09:47:34 -0300

    array list

--- src/Colections.kt
+++ src/Colections.kt
@@ -9,4 +9,19 @@ fun main() {
     val values: Array<Int> = arrayOf();
     empty = values.isEmpty();
     println(empty);
+    println(cities.size);
+
+    val fruits = ArrayList<String>();
+    println("Size of fruits " + fruits.size);
+    fruits.add("Banana");
+    fruits.add("Morango");
+    fruits.add("Manga");
+    fruits.add("Manga");
+    println("Size of fruits " + fruits.size);
+    println(fruits.contains("Uva"));
+    println(fruits.contains("Manga"));
+    println(fruits);
+    println(fruits.remove("Manga"));
+    println(fruits.remove("Uva"));
+    println(fruits);
 }
