269965ae197e1dca41963e863d4a4f5caf90c5d9
guilherme.monteiro.espim@gmail.com <guilherme.monteiro.espim@gmail.com>
2024-06-25 10:43:52 -0300

    math operations

---
+++ src/Operations.kt
@@ -0,0 +1,34 @@
+fun main(args: Array<String>) {
+    var a = 10;
+    val b = 20;
+    var c = a + b;
+    println("Soma $c");
+
+    c = a - b;
+    println("Subtração $c");
+
+    c = a * b;
+    println("Multiplicação $c");
+
+    c = a / b;
+    println("Divisão $c");
+
+    c = a % 2;
+    println("Resto da divisão $c");
+
+    a += 5;
+    println("Soma composta $a");
+
+    a -= 3;
+    println("Subtração composta $a");
+
+    a /= 2;
+    println("Divisão composta $a");
+
+    a *= 3;
+    println("Multiplicação composta $a");
+
+    a %= 2;
+    println("Resto de divisão composta $a");
+
+}
