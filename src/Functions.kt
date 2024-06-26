c3dbe8b663e5e5d0f14ee5f9b910f18f4196c108
guilherme.monteiro.espim@gmail.com <guilherme.monteiro.espim@gmail.com>
2024-06-26 09:28:01 -0300

    functions

--- src/Functions.kt
+++ src/Functions.kt
@@ -1,7 +1,23 @@
 fun main(args: Array<String>) {
+    hello();
     sum(10, 20);
+    sum(a=20, b=40);
+    val x: Int = subtraction(10, 5);
+    println(x);
+    val y: Int = divide(10, 5);
+    print(y);
 }
 
 fun sum(a: Int, b: Int) {
     println("$a + $b = ${a + b}")
 }
+
+fun hello() {
+    println("Hello!");
+}
+
+fun subtraction(a: Int, b: Int): Int {
+    return a - b;
+}
+
+fun divide(a: Int, b: Int) = a / b;
