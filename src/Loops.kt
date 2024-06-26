882151b7189a1edf0b6d2e308bb70c9c00c8926c
guilherme.monteiro.espim@gmail.com <guilherme.monteiro.espim@gmail.com>
2024-06-26 08:40:25 -0300

    for loop

--- src/Loops.kt
+++ src/Loops.kt
@@ -1,12 +1,20 @@
 fun main(args: Array<String>) {
     var nums: List<Int> = listOf(1, 2, 3, 4, 5, 6);
-    var i = 0;
-    while(i < nums.size) {
+//    var i = 0;
+//    while(i < nums.size) {
+//        if (nums[i] % 2 == 0) {
+//            println("O número ${nums[i]} é par")
+//        } else {
+//            println("O número ${nums[i]} é ímpar");
+//        }
+//        i++;
+//    }
+
+    for (i in 0 until nums.size) {
         if (nums[i] % 2 == 0) {
             println("O número ${nums[i]} é par")
         } else {
             println("O número ${nums[i]} é ímpar");
         }
-        i++;
     }
 }
