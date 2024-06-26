fc51e7f66b276fc8f56a65bb6cc60ba9bf2556ff
guilherme.monteiro.espim@gmail.com <guilherme.monteiro.espim@gmail.com>
2024-06-26 08:43:50 -0300

    for in

--- src/Loops.kt
+++ src/Loops.kt
@@ -1,5 +1,5 @@
 fun main(args: Array<String>) {
-    var nums: List<Int> = listOf(1, 2, 3, 4, 5, 6);
+//    var nums: List<Int> = listOf(1, 2, 3, 4, 5, 6);
 //    var i = 0;
 //    while(i < nums.size) {
 //        if (nums[i] % 2 == 0) {
@@ -10,11 +10,24 @@ fun main(args: Array<String>) {
 //        i++;
 //    }
 
-    for (i in 0 until nums.size) {
-        if (nums[i] % 2 == 0) {
-            println("O número ${nums[i]} é par")
-        } else {
-            println("O número ${nums[i]} é ímpar");
-        }
+//    for (i in 0 until nums.size) {
+//        if (nums[i] % 2 == 0) {
+//            println("O número ${nums[i]} é par")
+//        } else {
+//            println("O número ${nums[i]} é ímpar");
+//        }
+//    }
+
+    val fruits: List<String> = listOf("Maçã", "Uva", "Laranja", "Melancia");
+
+//    var i = 0;
+//
+//    while (i < fruits.size) {
+//        println(fruits[i]);
+//        i++;
+//    }
+
+    for (fruit: String in fruits) {
+        println(fruit);
     }
 }
