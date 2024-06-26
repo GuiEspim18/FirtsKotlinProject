7b74a96d625cd3db35594abf6bd083728a41d5ee
guilherme.monteiro.espim@gmail.com <guilherme.monteiro.espim@gmail.com>
2024-06-26 09:44:31 -0300

    map

--- src/MapFilterReduce.kt
+++ src/MapFilterReduce.kt
@@ -1,7 +1,7 @@
 fun main(args: Array<String>) {
 //    val nums: List<Int> = listOf(1, 2, 3, 4, 5, 6, 7, 8, 9);
 //    val pairs: List<Int> = nums.filter {
-//        it % 2 == 0
+//        it % 2 == 0;
 //    };
 //    println(pairs);
 
@@ -10,4 +10,11 @@ fun main(args: Array<String>) {
         it.startsWith("M");
     };
     println(fruitsM);
+
+    println("---- Utilizando o map ----");
+
+    val fruitsUpper: List<String> = fruits.map {
+        it.uppercase();
+    };
+    println(fruitsUpper);
 }
