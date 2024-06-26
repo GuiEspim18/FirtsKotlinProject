1a23921168b6f44c4d30d05a84d4eabc62d073be
guilherme.monteiro.espim@gmail.com <guilherme.monteiro.espim@gmail.com>
2024-06-26 09:40:40 -0300

    filter with string

--- src/MapFilterReduce.kt
+++ src/MapFilterReduce.kt
@@ -1,7 +1,13 @@
 fun main(args: Array<String>) {
-    val nums: List<Int> = listOf(1, 2, 3, 4, 5, 6, 7, 8, 9);
-    val pairs: List<Int> = nums.filter {
-        it % 2 == 0
+//    val nums: List<Int> = listOf(1, 2, 3, 4, 5, 6, 7, 8, 9);
+//    val pairs: List<Int> = nums.filter {
+//        it % 2 == 0
+//    };
+//    println(pairs);
+
+    val fruits: List<String> = listOf("Uva", "Pera", "Maçã", "Laranja", "Manga", "Mamão");
+    val fruitsM: List<String> = fruits.filter {
+        it.startsWith("M");
     };
-    println(pairs);
+    println(fruitsM);
 }
