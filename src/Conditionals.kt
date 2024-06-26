9c239829b0653331ea98803eec8b2cf6b78e226c
guilherme.monteiro.espim@gmail.com <guilherme.monteiro.espim@gmail.com>
2024-06-26 08:14:07 -0300

    conditional if else

---
+++ src/Conditionals.kt
@@ -0,0 +1,18 @@
+fun main(args: Array<String>) {
+    /* val age: Int = 14;
+    if (age >= 18) {
+        println("Maior de idade!");
+    } else {
+        println("Menor de idade!");
+    } */
+
+    val color: String = "azul";
+
+    if (color == "vermelho") {
+        println("Você escolheu vermelho");
+    } else if (color == "azul") {
+        println("Você escolheu azul");
+    } else {
+        println("Você escolheu amarelo");
+    }
+}
